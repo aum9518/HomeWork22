ca3f2c9378ceb94a8c6f9184fd6ad463cc37db60
aum9518 <abduganiuulumanasbek@gmail.com>
2023-03-05 19:03:31 +0600

    add HomeWork22

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        FileWriter songs = new FileWriter("song.txt");
+        songs.write("We were good, we were gold\n" +
+                "Kinda dream that can’t be sold\n" +
+                "We were right ’til we weren’t\n" +
+                "Built a home and watched it burn\n" +
+                "" +
+                "" +
+                "Mmm, I didn’t wanna leave you, I didn’t wanna lie\n" +
+                "Started to cry, but then remembered I\n" +
+                "" +
+                "" +
+                "I can buy myself flowers\n" +
+                "Write my name in the sand\n" +
+                "Talk to myself for hours\n" +
+                "Say things you don’t understand\n" +
+                "I can take myself dancing\n" +
+                "And I can hold my own hand\n" +
+                "Yeah, I can love me better than you can\n");
+        songs.close();
+
+        try {
+            Song song = new Song("Flowers", "Miley Cyrus", songs);
+            System.out.println("Title: " + song.getTitle() + "\nAuthor: " + song.getAuthor());
+            FileReader fileReader = new FileReader("song.txt");
+            BufferedReader bf = new BufferedReader(fileReader);
+            String line;
+            while ((line = bf.readLine()) != null) {
+                System.out.println(line);
+            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
--- src/Song.java
+++ src/Song.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Song {
+import java.io.FileWriter;
+
+public class Song {
+    private String title;
+    private String author;
+    private FileWriter text;
+
+    public Song() {
+    }
+
+    public Song(String title, String author, FileWriter text) {
+        this.title = title;
+        this.author = author;
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public FileWriter getText() {
+        return text;
+    }
+
+    public void setText(FileWriter text) {
+        this.text = text;
+    }
 }
